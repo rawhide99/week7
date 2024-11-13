f2133fa8f4199e33d14d4fd2d566f990c819c3bd
Dexter <ramya.austrax239@gmail.com>
2024-11-13 18:03:47 +0530

    Cpp files added

---
+++ cpp/vector1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <char> vowels {'a','e','i','o','u'};
+    cout << "The first vowel is " << vowels[0] << endl;
+    cout << "The last vowel is " << vowels[4] << endl;
+
+    vector <int> test_scores(3);    // All 3 elements initialized to zero.
+    vector <int> test_scores(3,100);    // All 3 elements initialized to Hundred.
+
+    vector <int> marks {100,96,89};
+    cout << "The marks at 1st position is " << marks[0] << endl;
+    cout << "The marks at 2nd position is " << marks[1] << endl;
+    cout << "The marks at 3rd position is " << marks[2] << endl;
+
+    // Marks using vector syntax
+
+    cout << "Marks using vector syntax: " << endl;
+
+    cout << "The marks at 1st position is " << marks.at(0) << endl;
+    cout << "The marks at 2nd position is " << marks.at(1) << endl;
+    cout << "The marks at 3rd position is " << marks.at(2) << endl;
+    cout << "The size of vector marks is " << marks.size() << endl;
+
+
+}
